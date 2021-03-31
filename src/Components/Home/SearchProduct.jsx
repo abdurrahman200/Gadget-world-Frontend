0f3467fb2bf1db9e51ae00548451ae9c09708dca
Abdur rahman <abdur.rahman200301@gmail.com>
2021-03-31 22:14:46 +0600

    style chnage

--- src/Components/Home/SearchProduct.jsx
+++ src/Components/Home/SearchProduct.jsx
@@ -1,15 +1,18 @@
 import { Card, Col, Input, Row } from 'antd';
 import Layout from 'antd/lib/layout/layout';
 import React from 'react';
+import { Typography } from 'antd';
+const { Title } = Typography;
 
 const { Search } = Input;
 
 const SearchProduct = () => {
     return (
-        <Layout style={{padding: '2rem 0'}}>
+        <Layout style={{ padding: '2rem 0' }}>
             <Row justify="center">
                 <Col xs={22} sm={20} md={18} lg={16} xl={14} xxl={12}>
                     <Card>
+                        <Title style={{ textAlign: 'center' }}>  Gadget World Online Shopping Store </Title>
                         <Search placeholder="Search Your Product" allowClear enterButton="Search" size="large" />
                     </Card>
                 </Col>
